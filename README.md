026530ee3b2c80cd836cb874ba87db9b6513dddc
Tetora186 <krystian.czerczak@gmail.com>
2021-07-10 20:15:02 +0200

    Merge branch 'master' of https://github.com/Tetora186/Moje_wgitypociny

---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# Moje_wypociny
+# Tu są moje programy które działają. Te nie działające nadal próbuję usprawnić i wrzucę kiedy je zbuduję.
+=======
+# Moje_wypociny
+
+>>>>>>> e3a2eeaaddce6d61c7d6a1b5c03bc2eabb318a5f
---
+++ kalkulator.py
@@ -0,0 +1,49 @@
+<<<<<<< HEAD
+print("Witaj w moim kalkulatorze.\nJakie działanie będziesz chciał przeprowadzać?")
+
+while(True):
+    znak = input("+ dodawanie, - odejmowanie, * mnożenie, / dzielenie, ** podnieś do potęgi, x zakończ ")
+
+    if (znak == '+'):
+        print(float(input("Wprowadź 1. liczbę: ")) + float(input("Wprowadź 2. liczbę: ")))
+
+    elif (znak == '-'):
+        print(float(input("Wprowadź 1. liczbę: ")) - float(input("Wprowadź 2. liczbę: ")))
+
+    elif (znak == '*'):
+        print(float(input("Wprowadź 1. liczbę: ")) * float(input("Wprowadź 2. liczbę: ")))
+
+    elif (znak == '/'):
+        print(float(input("Wprowadź 1. liczbę: ")) / float(input("Wprowadź 2. liczbę: ")))
+
+    elif (znak == '**'):
+        print(float(input("Wprowadź liczbę: ")) ** float(input("Do jakiej potęgi podnieść liczbę: ")))
+
+    elif (znak == 'x'):
+        break
+
+    else:
+=======
+print("Witaj w moim kalkulatorze.\nJakie działanie będziesz chciał przeprowadzać?")
+
+while(True):
+    znak = input("+ dodawanie, - odejmowanie, * mnożenie, / dzielenie, x zakończ ")
+
+    if (znak == '+'):
+        print(int(input("Wprowadź 1. liczbę: ")) + int(input("Wprowadź 2. liczbę: ")))
+
+    elif (znak == '-'):
+        print(int(input("Wprowadź 1. liczbę: ")) - int(input("Wprowadź 2. liczbę: ")))
+
+    elif (znak == '*'):
+        print(int(input("Wprowadź 1. liczbę: ")) * int(input("Wprowadź 2. liczbę: ")))
+
+    elif (znak == '/'):
+        print(int(input("Wprowadź 1. liczbę: ")) / int(input("Wprowadź 2. liczbę: ")))
+    
+    elif (znak == 'x'):
+        break
+
+    else:
+>>>>>>> e3a2eeaaddce6d61c7d6a1b5c03bc2eabb318a5f
+        print("Nierozpoznana funkcja")
